dc4c72c018223cbf0858a0a628ceb1363eea0293
Sebastian <sebastian.lillon@alumnos.uv.cl>
2020-06-04 23:21:23 +0000

    pregunta 1 v0.1

--- stats.sh
+++ stats.sh
@@ -16,37 +16,45 @@ printf "Directorio busqueda: %s\n" $1
 
 executionSummaryFiles=(`find $searchDir -name '*.txt' -print | sort | grep execution | grep -v '._'`)
 
-sumaInicial="sumaInicial.txt"
-rm -f $sumaInicial
-sumaFinal="sumaFinal.txt"
-rm -f $sumaFinal
+sumaInicialExec="sumaInicialExec.txt"
+rm -f $sumaInicialExec
+
+sumaInicialMem="sumaInicialMem.txt"
+rm -f $sumaInicialMem
+
 for i in ${executionSummaryFiles[*]};
 do
 	printf "> %s\n" $i
 	timeExec1=$(cat $i | tail -n +2 | cut -d ':' -f 6 )
 	timeExec2=$(cat $i | tail -n +2 | cut -d ':' -f 7 )
 	timeExec3=$(cat $i | tail -n +2 | cut -d ':' -f 8 )
-	printf $timeExec1"\n" >> $sumaInicial
-	printf $timeExec2"\n" >> $sumaInicial
-	printf $timeExec3"\n" >> $sumaInicial
-	cmdSum=$(cat $sumaInicial | awk 'BEGIN {n=0} {n =+ $1}; END{print n}')
-	printf $cmdSum"\n" >> $sumaFinal
+	echo $timeExec1 >> $sumaInicialExec
+	echo $timeExec2 >> $sumaInicialExec
+	echo $timeExec3 >> $sumaInicialExec
+	memoryUsage=$(cat $i | tail -n +2 | cut -d ':' -f 9)
+	echo $memoryUsage >> $sumaInicialMem
 
 done
 
-cmdPromMaxMin=$(cat $sumaFinal | awk 'BEGIN{ min=2**63-1; max=0}{ if($1<min){min=$1};\
+cmdPromMaxMinExec=$(cat $sumaInicialExec | awk 'BEGIN{ min=2**63-1; max=0}{ if($1<min){min=$1};\
 								if($1>max){max=$1};\
 							        total+=$1; count+=1;\
 						           	} \
-							 	END { print total/count, max, min}')
+							 	END { print total":"total/count":"min":"max}')
+
+cmdPromMaxMinMem=$(cat $sumaInicialMem  | awk 'BEGIN{ min=2**63-1; max=0}{ if($1<min){min=$1};\
+                                                                if($1>max){max=$1};\
+                                                                total+=$1; count+=1;\
+                                                                } \
+                                                                END { print total":"total/count":"min":"max}')
 
 
 metricsFile="metrics.txt"
 rm -f $metricsFile
 
 
-echo $cmdPromMaxMin
+echo $cmdPromMaxMinExec >> $metricsFile
+echo $cmdPromMaxMinMem >> $metricsFile
 
-printf $cmdPromMaxMin"\n" >> $metricsFile
 
 
